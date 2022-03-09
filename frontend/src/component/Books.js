d8779ec886e3d7e1cb050f1f5aa5d4f0d6dd2d15
HudaAlswilmi <Huda.Alswilmi.rae.js@tuwaiq.edu.sa>
2022-03-09 22:39:11 +0300

    2

--- frontend/src/component/Books.js
+++ frontend/src/component/Books.js
@@ -1,10 +1,56 @@
-import React ,{useState,useEffect} from 'react'
+import React  from 'react'
+import { Card ,CardGroup} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Book.css'
+import axios from "axios";
 
 export default function Books() {
-  const [Books, setBooking] = useState([]);
-
+ 
   return (
-    <div>Books</div>
-    
-  )
-}
+    <div className="div1">
+
+<CardGroup>
+  <Card className='book1'>
+    <Card.Img variant="top" src="https://www.neelwafurat.com/images/lb/abookstore/covers/normal/308/308362.jpg" height="500px" width="300px" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card className='book1'>
+    <Card.Img variant="top" src="https://www.neelwafurat.com/images/lb/abookstore/covers/normal/308/308362.jpg" height="500px" width="300px" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card className='book1'>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+</CardGroup>
+   
+    </div>
+  )}
--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
  const Home = () => {
   return (
     <>    
-    <h1>home ...</h1>
-    <h1>home ...</h1>
-    <h1>home ...</h1>
+  <Carousel>
+  <Carousel.Item interval={1000}>
+    <img
+      className="d-block w-100 "
+      src="https://images.pexels.com/photos/159711/books-bookstore-book-reading-159711.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+     height="600px" width="600px"
+      alt="First slide"
+    />
+
+  </Carousel.Item>
+  <Carousel.Item interval={500}>
+    <img
+      className="d-block w-100"
+      src="https://images.pexels.com/photos/2465877/pexels-photo-2465877.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+      height="600px" width="600px"
+
+      alt="Second slide"
+    />
+
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://images.pexels.com/photos/1130980/pexels-photo-1130980.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+           height="600px" width="600px"
+
+      alt="Third slide"
+    />
+ 
+  </Carousel.Item>
+</Carousel>
     </>
   )
 }
